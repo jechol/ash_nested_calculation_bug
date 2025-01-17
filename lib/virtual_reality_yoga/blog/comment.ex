e56f7cb381c9c4e89b6f630aee634a53062b13c2
Jechol Lee <mr.jechol@gmail.com>
2025-01-17 15:38:51 +0900

    Add attributes

--- lib/virtual_reality_yoga/blog/comment.ex
+++ lib/virtual_reality_yoga/blog/comment.ex
@@ -2,4 +2,19 @@ defmodule VirtualRealityYoga.Blog.Comment do
   use Ash.Resource,
     otp_app: :virtual_reality_yoga,
     domain: VirtualRealityYoga.Blog
+
+  alias VirtualRealityYoga.Blog.Post
+
+  actions do
+    default_accept :*
+    defaults [:create, :read, :update, :destroy]
+  end
+
+  attributes do
+    uuid_primary_key :id
+  end
+
+  relationships do
+    belongs_to :post, Post
+  end
 end
--- lib/virtual_reality_yoga/blog/post.ex
+++ lib/virtual_reality_yoga/blog/post.ex
@@ -2,4 +2,19 @@ defmodule VirtualRealityYoga.Blog.Post do
   use Ash.Resource,
     otp_app: :virtual_reality_yoga,
     domain: VirtualRealityYoga.Blog
+
+  alias VirtualRealityYoga.Blog.Comment
+
+  actions do
+    default_accept :*
+    defaults [:create, :read, :update, :destroy]
+  end
+
+  attributes do
+    uuid_primary_key :id
+  end
+
+  relationships do
+    has_many :comments, Comment
+  end
 end
